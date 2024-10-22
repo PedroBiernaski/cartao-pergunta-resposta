6535c5b2441acbb90bc8916194f2dc44faab9b33
PedroBiernaski <137187178+PedroBiernaski@users.noreply.github.com>
2024-10-22 15:36:12 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciências',
+    'Qual é o maior órgão do corpo humano?',
+    'A pele'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o continente mais frio da Terra?',
+    'A Antártida'
+)
+
+criaCartao(
+    'História',
+    'Quem pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Natureza',
+    'Qual é o processo pelo qual as plantas produzem seu próprio alimento?',
+    'Fotossíntese'
+)
